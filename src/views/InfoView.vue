2665b04c261345b02e3e1881bbf81c05c2dcc5df
kyjak <mkrsjak@hotmail.com>
2026-04-22 09:24:58 +0200

    pravne info doplnene

--- src/views/InfoView.vue
+++ src/views/InfoView.vue
@@ -3,13 +3,23 @@
     <div class="content-container text-container">
       <h1>Právní informace</h1>
       <p>
-        <strong>VALOR FORTIS a.s.</strong> je nezávislá společnost a není propojena
-        se společností SIX Financial Information AG ani s jejími ochrannými známkami
-        (například VALORDATA).
+        <strong>VALOR FORTIS a.s.</strong> je nezávislá společnost a není
+        propojena se společností SIX Financial Information AG ani s jejími
+        ochrannými známkami (například VALORDATA).
       </p>
       <p>
-        <strong>VALOR FORTIS a.s.</strong> je nezávislá společnost a není propojena
-        se společností BNP Paribas Fortis.
+        <strong>VALOR FORTIS a.s.</strong> je nezávislá společnost a není
+        propojena se společností BNP Paribas Fortis.
+      </p>
+      <br /><br />
+      <p>
+        <strong>VALOR FORTIS a.s.</strong> is a independent company which is not
+        affiliated with the company SIX Financial Information AG or its
+        trademarks (e.g. VALORDATA).
+      </p>
+      <p>
+        <strong>VALOR FORTIS a.s.</strong> is a independent company which is not
+        affiliated with the company BNP Paribas Fortis.
       </p>
     </div>
   </section>
